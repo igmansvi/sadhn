0ae289505e4939b7e13be2e25fc4c329e5febaf3
Tejasvita <ttejasvita@gmail.com>
2025-12-10 16:37:46 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
-    <div>Hello, world!</div>
-  )
-}
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
 
 export default App;
